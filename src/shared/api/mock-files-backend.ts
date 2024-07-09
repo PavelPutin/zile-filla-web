22f90349b0e0d8e5f7310daa589f6861d1d079b2
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-07-09 13:41:14 +0300

    chore: remove mock classes

--- src/shared/api/mock-files-backend.ts
+++
@@ -1,7 +0,0 @@
-import { TextFileContent } from "../model/text-file-content";
-
-export const FILES: { [key: string]: TextFileContent } = {
-  "/etc/photo.txt": {
-    content: "Картинка с котиком в скафандре, сидящем на неизвестной планете в ожидании хозяина."
-  }
-}
