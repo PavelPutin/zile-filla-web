0932c4ba8f96244a4e9125a1e749eb114a58e03c
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-07-10 14:05:15 +0300

    feat: add logo in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,21 @@
 "use client"
 
+import { Stack } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <AppBar color="transparent" position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
-          <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
-            Zile Filla
-          </Typography>
+          <Stack alignItems="center" direction="row" gap={1}>
+            <Image src={"/images/ZileFilla.png"} alt="Логотип" width={50} height={50} />
+            <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>Zile Filla</Typography>
+          </Stack>
         </Toolbar>
       </AppBar>
     </Box>
