7f5ce5f3d16f844574e9b1db018be45bb8bfce70
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-07-11 16:10:46 +0300

    fix: remove caching

--- src/shared/api/http-view-files-api.ts
+++ src/shared/api/http-view-files-api.ts
@@ -5,7 +5,7 @@ export class HttpViewFilesApi implements ViewFilesApi {
   async getFileContent(path: string): Promise<TextFileContent> {
     console.log(`start view file(${path})`);
     console.log(`fetch http://${process.env.BACKEND_HOST}/visit${path}`)
-    const response = await fetch(`http://${process.env.BACKEND_HOST}:8080/view${path}`);
+    const response = await fetch(`http://${process.env.BACKEND_HOST}:8080/view${path}`, {cache: "no-store"});
     if (!response.ok) {
       throw await response.json();
     }
